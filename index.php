d9756bdf738cbb0d76c78e5a337120a17d83dd45
Pedro Gomes <up202108825@up.pt>
2023-05-31 19:59:43 +0100

    authentication page done

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header('Location: pages/landing_page.php'); ?>
---
+++ templates/auth_template.php
@@ -0,0 +1,50 @@
+<?php function draw_head()
+{ ?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Authentication</title>
+        <link href='/../style/style.css' rel='stylesheet'>
+    </head>
+
+    <body>
+<?php } ?>
+
+<?php function draw_auth() { ?>
+<div id="auth">
+    <form id="login" class="auth" method="post" action="/../actions/login.php">
+        <h2>Already have an account? Welcome back!</h2>
+        <span class='input-name'>username / e-mail</span>
+        <input type="text" name="usernameemail" autocomplete="off">
+        <span class='input-name'>password</span>
+        <input type="password" name="password" autocomplete="off">
+        <input type="submit" value="Sign in">
+    </form>
+
+    <form id="signup" class="auth" method="post" action="/../actions/signup.php">
+        <h2>New here? Join us!</h2>
+        <span class='input-name'>username</span>
+        <input name="username" type="text" autocomplete="off">
+        <span class='input-name'>e-mail</span>
+        <input name="email" type="email" autocomplete="off">
+        <span class='input-name'>password</span>
+        <input name="password" type="password" autocomplete="off">
+        <input type="submit" value="Sign up">
+    </form>
+</div>
+</body>
+
+</html>
+<?php } ?>
+
+<?php function draw_alerts(Session $session){
+    $messages = $session->getMessages();
+
+    foreach($messages as $message){
+        if($message['type'] == 'error')
+            echo '<script>window.onload = function() { alert("' . $message['text'] . '"); };<script>';
+    }
+}
+?>
